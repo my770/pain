bad37409c272e43515470b2eddd61a9ce701d235
Moh Younis <mohamed.younis@doctors.org.uk>
2024-09-28 11:48:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("/painApp/sw.js").then(
+      function (registration) {
+        console.log(
+          "Service Worker registered with scope:",
+          registration.scope
+        );
+      },
+      function (error) {
+        console.log("Service Worker registration failed:", error);
+      }
+    );
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   var coll = document.getElementsByClassName("collapsible");
 
